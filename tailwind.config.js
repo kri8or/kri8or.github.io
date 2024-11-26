12d09b587fe92f0a3fd421e8a055fd1349f38706
znup <znup@znup-mac17.lan>
2024-11-27 00:17:36 +0100

    now it's ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./layouts/**/*.html",          // Include all layouts
+    "./content/**/*.md",            // Include all content files
+    "./themes/polish-davmar/**/*.html",  // Include templates from the theme
+  ],
   theme: {
     extend: {},
   },
