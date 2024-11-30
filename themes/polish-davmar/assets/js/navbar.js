48663652c22795d95e18e2ad5aef5d4c2ad700e2
znup <znup@znup-mac17.lan>
2024-12-01 00:52:32 +0100

    dark mode

---
+++ themes/polish-davmar/assets/js/navbar.js
@@ -0,0 +1,28 @@
+//hamburger menu and darkmode
+
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById('menu-btn')
+    const menu = document.getElementById('menu')
+    const darkButton = document.getElementById('dark_button');
+    const moon = document.getElementById('moon')
+    const sun = document.getElementById('sun')
+    const body = document.body;
+
+    btn.addEventListener('click', navToggle)
+
+    // Toggle Mobile Menu
+    function navToggle() {
+        btn.classList.toggle('open')
+        menu.classList.toggle('flex')
+        menu.classList.toggle('hidden')
+    }
+
+    darkButton.addEventListener('click', darkToggle)
+
+    // Toggle dark mode
+    function darkToggle() {
+        body.classList.toggle('dark')
+        moon.classList.toggle('fade')
+        sun.classList.toggle('fade')
+    }
+})
